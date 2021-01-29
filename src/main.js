4cf89fe82883173fff05485636e49e4dbe96a6d2
Dicky Budianto <dickybudianto@usahakreatif.co.id>
2021-01-29 09:59:53 +0700

    update port sandbox

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import vuetify from './plugins/vuetify';
 
 Vue.use(require("moment"));
 Vue.config.productionTip = false
-Vue.prototype.$urlLink = 'http://192.168.0.96:8443'
-Vue.prototype.$localIp = 'http://192.168.0.228:8109/#/'
+Vue.prototype.$urlLink = 'https://sandbox.api.ultipay.id:8443'
+Vue.prototype.$localIp = 'http://sandbox.dashboard.ultipay.id:8109/#/'
 Vue.prototype.$flowEntry = 'itDashboard'
 
 //format = itDashboard:key
